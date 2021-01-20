ab8f3708048136c1afc491f0e738a43f004eed74
Winicius <wine.xavier@gmail.com>
2021-01-19 22:37:55 -0300

    add and test first component styles

--- client/src/components/Main/index.tsx
+++ client/src/components/Main/index.tsx
@@ -1,7 +1,18 @@
+import { Description, Illustration, Logo, Title, Wrapper } from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <Wrapper>
+    <Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado"
+    ></Logo>
+    <Title>React Avançado</Title>
+    <Description>Typesript, ReactJS, NextJS e Styled Components</Description>
+    <Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente pra uma tela com código."
+    />
+  </Wrapper>
 )
 
 export default Main
