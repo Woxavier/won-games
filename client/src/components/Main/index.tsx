28fae3dc0e91f3c01c33c0082d4872fb030b1086
Winicius <wine.xavier@gmail.com>
2021-01-21 01:19:59 -0300

    add and config integration with storybook

--- client/src/components/Main/index.tsx
+++ client/src/components/Main/index.tsx
@@ -1,13 +1,16 @@
 import { Description, Illustration, Logo, Title, Wrapper } from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'React Avançado',
+  description = 'Typesript, ReactJS, NextJS e Styled Components'
+}) => (
   <Wrapper>
     <Logo
       src="/img/logo.svg"
       alt="Imagem de um átomo e React Avançado escrito ao lado"
     ></Logo>
-    <Title>React Avançado</Title>
-    <Description>Typesript, ReactJS, NextJS e Styled Components</Description>
+    <Title>{title}</Title>
+    <Description>{description}</Description>
     <Illustration
       src="/img/hero-illustration.svg"
       alt="Um desenvolvedor de frente pra uma tela com código."
